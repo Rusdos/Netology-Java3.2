2d65a052126d31a2ec98f6d4f3d71335eabc227c
Rusdos <mr.Nurgaleev@gmail.com>
2020-08-26 11:11:27 +0500

    Editing varieble names

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,13 @@ import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
         double balance = 100.97;
-        double replenishment_amount = 522.37;
+        double replenishmentAmount = 522.37;
         int bonus;
-        if (replenishment_amount > 1000) {
-            bonus = (int)replenishment_amount/100;
+        if (replenishmentAmount > 1000) {
+            bonus = (int)replenishmentAmount/100;
             System.out.println("Ваш бонус: "+bonus);
         } else System.out.println("Пополните баланс более чем на 1000 руб. и полуите от нас бонус 1 руб. за каждые 100 рублей!!!");
-        balance = replenishment_amount+balance;
-        /////////////////////////////////////////
-        String pattern = "##0.00";
-        DecimalFormat decimalFormat = new DecimalFormat(pattern);
-        String New_balance = decimalFormat.format(balance);
-        System.out.println("Ваш текущий баланс "+New_balance);
-        //System.out.println("Ваш текущий баланс: "+balance);
+        balance = replenishmentAmount+balance;
+
     }
 }
